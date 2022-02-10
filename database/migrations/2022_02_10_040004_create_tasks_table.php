43151bfa48f272a9ea9c941d60b873c2fe9b14d2
pirs <roman2027@internet.ru>
2022-02-10 10:25:00 +0600

    created tables

--- database/migrations/2022_02_10_040004_create_tasks_table.php
+++ database/migrations/2022_02_10_040004_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('api_token');
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->text('body');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 }
